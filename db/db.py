e4731f0fdad5e9241e94bff87ddf8bc2c1b5b848
etimepass123-tech <etimepass123@gmail.com>
2026-01-04 21:12:08 +0530

    updated

--- db/db.py
+++ db/db.py
@@ -1,9 +1,13 @@
+import streamlit as st
 import mysql.connector
 
 def get_connection():
+    # This reads the database info from the "Secrets" area of Streamlit Cloud
     return mysql.connector.connect(
-        host="127.0.0.1",
-        user="root",
-        password="",
-        database="agri_streamlit"
+        host=st.secrets["db_host"],
+        port=int(st.secrets["db_port"]),
+        user=st.secrets["db_user"],
+        password=st.secrets["db_password"],
+        database=st.secrets["db_name"]
     )
+    
