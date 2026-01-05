b1cd4015376feb3e26d81fba94965c64c181c88b
etimepass123-tech <etimepass123@gmail.com>
2026-01-05 10:37:01 +0530

    Add SQLAlchemy engine creation for Pandas
    Added a new function to create a SQLAlchemy engine for Pandas operations.

--- db/db.py
+++ db/db.py
@@ -1,9 +1,9 @@
 import streamlit as st
+from sqlalchemy import create_engine
 import mysql.connector
 
+# We keep this for cursor operations (Admin tasks)
 def get_connection():
-    # This line tells the app: "Don't look at 127.0.0.1. 
-    # Look at the 'Secrets' box in my Streamlit settings instead!"
     return mysql.connector.connect(
         host=st.secrets["db_host"],
         port=int(st.secrets["db_port"]),
@@ -11,3 +11,12 @@ def get_connection():
         password=st.secrets["db_password"],
         database=st.secrets["db_name"]
     )
+
+# WE ADD THIS for Pandas operations (Reading Data)
+def get_engine():
+    conn_url = (
+        f"mysql+mysqlconnector://{st.secrets['db_user']}:"
+        f"{st.secrets['db_password']}@{st.secrets['db_host']}:"
+        f"{st.secrets['db_port']}/{st.secrets['db_name']}"
+    )
+    return create_engine(conn_url)
